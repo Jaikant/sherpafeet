f6029615fe870615d76cfd4976e72dff6a5fca19
jai <jaikant@gmail.com>
2018-10-08 12:13:29 +0530

    Cosmetic changes

--- src/components/Trekpopover.js
+++ src/components/Trekpopover.js
@@ -1,33 +1,24 @@
-
-import React, { Component } from 'react';
-import { Popover, Button } from 'antd';
-
-
-
-
-
+import React from "react";
+import { Popover } from "antd";
 
 function Popovertrek(props) {
-    const content = (
-        <div>
-            <p>Content</p>
+  const content = (
+    <div>
+      <p>Content</p>
+    </div>
+  );
 
-        </div>
-    );
-  
-
-    return (
-        <div 
-            style={{ marginLeft:'0px', clear: 'both', whiteSpace: 'nowrap' }}
-        >   
-            <Popover content={props.content} title={props.title} arrowPointAtCenter trigger="hover">
-           
-                <div >{props.children}</div>
-                {/* <Button style={{ borderColor: '#FFFAFA' }}></Button> */}
-            </Popover>
-            </div>
-        
-
-    )
+  return (
+    <div style={{ marginLeft: "0px", clear: "both", whiteSpace: "nowrap" }}>
+      <Popover
+        content={props.content}
+        title={props.title}
+        arrowPointAtCenter
+        trigger="hover"
+      >
+        <div>{props.children}</div>
+      </Popover>
+    </div>
+  );
 }
 export default Popovertrek;
